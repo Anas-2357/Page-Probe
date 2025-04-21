5ad68525384adad6e853fcb36c7a783e5c1173ff
Anas <anasansari2357@gmail.com>
2025-04-21 22:50:50 +0530

    add load speed

--- popup.js
+++ popup.js
@@ -1,6 +1,30 @@
 setInterval(() => {
     chrome.runtime.sendMessage({ type: 'REQUEST_PERF_DATA' }, (response) => {
-        document.getElementById('loadTime').textContent = `Load Time: ${response.loadTime} ms`;
-        document.getElementById('domCount').textContent = `DOM Elements: ${response.domCount}`;
+        if(response.loadTime !== undefined){
+            document.getElementById('loadTime').textContent = `Load Time: ${response.loadTime} ms`;
+            document.getElementById('domCount').textContent = `DOM Elements: ${response.domCount}`;
+        }
+
+        let loadSpeedStatus = '';
+        let color = '';
+
+        if (response.loadTime < 1000) {
+            loadSpeedStatus = 'Good';
+            color = 'green';
+        } else if (response.loadTime >= 1000 && response.loadTime < 2500) {
+            loadSpeedStatus = 'Acceptable';
+            color = 'orange';
+        } else {
+            loadSpeedStatus = 'Bad';
+            color = 'red';
+        }
+        
+
+        const loadSpeedElement = document.getElementById('loadSpeedStatus');
+        loadSpeedElement.textContent = `Load Speed: ${loadSpeedStatus}`;
+        loadSpeedElement.style.color = color;
+
+
+
       });
-}, 50)
+}, 200)
