5ef9823f03045a2d8154d8ee51f9ef017f50b500
Anas <anasansari2357@gmail.com>
2025-04-21 22:13:11 +0530

    add polling popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
-chrome.runtime.sendMessage({ type: 'REQUEST_PERF_DATA' }, (response) => {
-    document.getElementById('loadTime').textContent = `Load Time: ${response.loadTime} ms`;
-    document.getElementById('domCount').textContent = `DOM Elements: ${response.domCount}`;
-  });
-  
+setInterval(() => {
+    chrome.runtime.sendMessage({ type: 'REQUEST_PERF_DATA' }, (response) => {
+        document.getElementById('loadTime').textContent = `Load Time: ${response.loadTime} ms`;
+        document.getElementById('domCount').textContent = `DOM Elements: ${response.domCount}`;
+      });
+}, 50)
